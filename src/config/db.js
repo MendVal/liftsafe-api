f4465579a691ed17bcea103df99e8cad3004d0e2
MendVal <mendivilcorrea1305@gmail.com>
2026-04-10 11:43:45 -0500

    Usar variables de entorno para conexión a MySQL

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-
-const host = process.env.MYSQLHOST || 'localhost';
-const port = process.env.MYSQLPORT || 3306;
-const user = process.env.MYSQLUSER || 'root';
-const password = process.env.MYSQLPASSWORD || '';
-// El nombre de la base de datos en Railway es 'railway'
-const database = process.env.MYSQLDATABASE || 'railway';
-
-console.log(' Conectando a BD:', { host, port, user, database });
-
 const pool = mysql.createPool({
-  host,
-  port,
-  user,
-  password,
-  database,
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || 3306,
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'liftsafe_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
