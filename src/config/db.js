c36007ec41b7fa71391f5f9ccd8ad038fbffe43e
MendVal <mendivilcorrea1305@gmail.com>
2026-04-10 11:27:33 -0500

    Forzar BD railway en Railway, liftsafe_db en local

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,20 @@
 const mysql = require('mysql2');
-
-// Cargar variables de entorno
 require('dotenv').config();
+
+
+const isRailway = !!process.env.MYSQLHOST;
 
 const host = process.env.MYSQLHOST || process.env.DB_HOST || 'localhost';
 const port = process.env.MYSQLPORT || process.env.DB_PORT || 3306;
 const user = process.env.MYSQLUSER || process.env.DB_USER || 'root';
 const password = process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '';
-const database = process.env.MYSQLDATABASE || process.env.DB_NAME || 'liftsafe_db';
 
-console.log(' Conectando a BD con:', { host, port, user, database }); 
+// Si está en Railway, forzar 'railway' como nombre de la BD
+const database = isRailway 
+  ? 'railway' 
+  : (process.env.MYSQLDATABASE || process.env.DB_NAME || 'liftsafe_db');
+
+console.log('🔌 Conectando a BD con:', { host, port, user, database, isRailway });
 
 const pool = mysql.createPool({
   host,
